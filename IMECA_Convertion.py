9e2c5834ccc553b19cbc8782dee6d3a64f4eefaf
DiegoGugo <diego.gugo@outlook.com>
2023-09-09 22:53:39 -0600

    Inicio de conversion de compuesto a IMECA

---
+++ IMECA_Convertion.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import os
+
+class IMECA_Convertion:
+    def __init__ (self, data_path):
+        self.data_path = data_path
+    
+    def convertion(value, compound):
+        
+    
